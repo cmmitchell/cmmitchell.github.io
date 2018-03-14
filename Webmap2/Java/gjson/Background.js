6e258988a8acb611cedf0ded23c8c772c9b1e916
Caleb <33265183+cmmitchell@users.noreply.github.com>
2018-03-13 22:48:39 -0400

    Add files via upload

---
+++ Webmap2/Java/gjson/Background.js
@@ -0,0 +1,32 @@
+var bak =
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 1, "Id": 0, "Shape_Length": 201113917.20469776, "Shape_Area": 2423814811083795.5, "SQ_MI_Cylind": 196907160.79550892, "Sq_Mi_Mercator": 935840130.58801091 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -179.99999550841457, -88.999999921611192 ], [ -179.99999550841457, 88.999999921611177 ], [ 179.99999550841471, 88.999999921611177 ], [ 179.99999550841471, -88.999999921611192 ], [ -179.99999550841457, -88.999999921611192 ] ] ] ] } }
+]
+}
+
+function back(feature) {
+   return {
+   fillColor: '#0099FF',
+   weight: 1,
+   opacity: 1,
+   color: '#0099FF',
+   weight: 1,
+   opacity: 1,
+   
+   };
+}
+
+function backblack(feature) {
+   return {
+   fillColor: '#3e3e3e',
+   weight: 10,
+   opacity: 1,
+   color: '#000000',
+   weight: 1,
+   opacity: 1,
+   
+   };
+}
---
+++ Webmap2/Java/gjson/scratchhh.js
@@ -0,0 +1,131 @@
+function countryclassic(feature){
+		switch (feature.properties.Classical_) {
+
+			}
+		};
+
+function countrystyle3600(feature){
+		switch (feature.properties.GC_3600) {
+
+			}
+		};		
+		
+		
+		
+function countrystyle4075(feature){
+		switch (feature.properties.GC_4075) {
+
+			}
+		};
+
+function countrystyle4080(feature){
+		switch (feature.properties.GC_4080) {
+
+			}
+		};
+		
+
+		
+function countrystyle4875(feature){
+		switch (feature.properties.GC_4875) {
+
+			}
+		};
+		
+
+		
+function countrystyle5125(feature){
+		switch (feature.properties.GC_5125) {
+
+			}
+		};
+		
+
+function countrystyle5400(feature){
+		switch (feature.properties.GC_5400) {
+
+			}
+		};		
+function countrystyle5500(feature){
+		switch (feature.properties.GC_5500) {
+
+			}
+		};
+
+
+		
+function countrystyle5780(feature){
+		switch (feature.properties.GC_5780) {
+
+			}
+		};
+function countrystyle5800(feature){
+		switch (feature.properties.GC_5800) {
+
+			}
+		};		
+function countrystyle5840(feature){
+		switch (feature.properties.GC_5840) {
+
+			}
+		};
+		
+function countrystyle5870(feature){
+		switch (feature.properties.GC_5870) {
+
+			}
+		};
+		
+
+		
+
+		
+
+		
+function countrystyle5900(feature){
+		switch (feature.properties.GC_5900) {
+
+			}
+		};
+		
+function countrystyle5920(feature){
+		switch (feature.properties.GC_5920) {
+
+	
+			}
+		};
+		
+function countrystyle5940(feature){
+		switch (feature.properties.GC_5940) {
+
+
+			}
+		};
+		
+function countrystyle5965(feature){
+		switch (feature.properties.GC_5965) {
+
+
+			}
+		};
+function countrystyle6200(feature){
+		switch (feature.properties.GC_6200) {
+
+			}
+		};
+function countrystyle6520(feature){
+		switch (feature.properties.GC_6520) {
+
+			}
+		};		
+
+
+		
+function countrymi(feature) {
+   return {
+   color: '#0099FF',
+   weight: .85,
+   opacity: .5,
+   
+   };
+}
